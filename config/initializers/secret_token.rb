6f94fb53d22390861578605f3d3101a77ad3a759
HouseKeeper <carl@house-keeping.com>
2012-11-08 22:04:33 +0800

    Initial Commit of Rails v3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meerkat::Application.config.secret_token = '72a5e21871e7a9642c6988de3a8b83a15983a79d1b67a687356fb26e3eed9679cc6d9cbc7b5b484e2ec1f90e23b6f395d7512abca3d0e1e2ee4ccb6bb84de76a'
